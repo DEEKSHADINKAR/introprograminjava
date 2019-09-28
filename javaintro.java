ab75af9cf6dd122ecae3df26622787d38a59220f
Deeksha Dinkar <43996281+DEEKSHADINKAR@users.noreply.github.com>
2019-09-28 22:18:04 +0530

    Add files via upload

---
+++ javaintro.java
@@ -0,0 +1,15 @@
+class javaintro
+{
+public static void main(String args[])
+{
+int a=1;
+int b=2;
+int c=3;
+a+=5;
+b*=4;
+c%=6;
+System.out.print("a="+a);
+System.out.print("b="+b);
+System.out.print("c="+c);
+}
+}
